c0bd9c4ca6110df8da386a13c84ee38d00d08102
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-09-08 16:09:21 +0300

    [App Struct] Add Port no

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 'use strict';
 const express = require('express');
 const app = express();
-let port = 4040;
+let port = 4040 || process.env.PORT;
 
 app.get('/', (req, res) => {
   res.send('This is our landing page');
