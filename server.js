48a1328b56f79ec307b29bc152a16a8908e497de
andela-gnyenyeshi <gertrude.nyenyeshi@andela.com>
2016-09-18 15:56:15 +0300

    [CI] Remove staging

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = express();
 let port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
-  res.send('This is our landing page Olaaaaa');
+  res.send('This is our landing page Welcome');
 });
 
 app.listen(port, () => {
